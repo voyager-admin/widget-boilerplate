39c749d698c165f104b82cf8f15fafa9f0e90e87
Christoph Schweppe <info@cschweppe.de>
2021-08-31 12:46:15 +0200

    Remove unnecessary files

--- resources/views/instructions.blade.php
+++
@@ -1,2 +0,0 @@
-<h2>Welcome to the most widget useless Plugin for Voyager</h2>
-<p>To install this plugin, just do nothing :)</p>
--- src/WidgetBoilerplateServiceProvider.php
+++ src/WidgetBoilerplateServiceProvider.php
@@ -9,13 +9,7 @@ class WidgetBoilerplateServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'widget-boilerplate');
         PluginsFacade::addPlugin(\VoyagerAdmin\WidgetBoilerplate\FirstWidget::class);
         PluginsFacade::addPlugin(\VoyagerAdmin\WidgetBoilerplate\SecondWidget::class);
-    }
-
-    public function register()
-    {
-        //
     }
 }
